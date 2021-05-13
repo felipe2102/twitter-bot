738d78ead61c7031fbbb16d9a9488cacb6a2eb94
Felipe Ribeiro <felipinhooliveira06@gmail.com>
2021-05-12 21:00:59 -0300

    GUI
    Adicionada interface gráfica PySimpleGUI

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import PySimpleGUI as sg
+import threading
 import tweepy
 from time import sleep
 
@@ -10,29 +12,53 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-def re():
-	search = "#goza"
-	search2 = "#goza"
+def retweet(word):
 	numberOfTweets = 10000
-	for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+	for tweet in tweepy.Cursor(api.search, word).items(numberOfTweets):
 		try:
 			tweet.retweet()
 			tweet.favorite()
 			print('Retweeted and liked this tweet')
 			sleep(5)
-		except tweepy.TweepError as e:
-			print(e.reason)
-		except StopIteration:
-		 	break
-	for tweet in tweepy.Cursor(api.search, search2).items(numberOfTweets):
-		try:
-			tweet.retweet()
-			tweet.favorite()
-			print('Retweeted and liked this tweet')
-			sleep(5)
-		except tweepy.TweepError as e:
-			print(e.reason)
+		except tweepy.TweepError as ex:
+			print(ex.reason)
 		except StopIteration:
 		 	break
 
-re()
+def home():
+	sg.theme('DarkGrey13')
+	layout = [
+		[sg.Text('Twitter Bot', size=(10, 1))],
+		[sg.Text('Word to search:')],
+		[sg.Input(key='word')],
+		[sg.Button("Confirm")],
+		[sg.Output(size=(30, 5))],
+		[sg.Button('Help'), sg.Exit()]
+		]
+	return sg.Window('Control', layout, element_justification='c', finalize=True)
+
+def help():
+	sg.theme('DarkGrey13')
+	layout = [
+		[sg.Text('If you see any issue, you can report at:')],
+		[sg.Text('https://github.com/felipe2102/twitter-bot/issues/new')],
+		[sg.Button('Return')]
+	]
+	return sg.Window('Help', layout, element_justification='c', finalize=True)
+
+index, ajuda = home(), None
+
+if __name__ == '__main__':
+	while True:
+		window, event, values = sg.read_all_windows()
+		if window == index and event == sg.WIN_CLOSED or event == 'Exit':
+			break
+		if window == index and event == 'Help':
+			ajuda = help()
+			index.hide()
+		if window == ajuda and event == 'Return':
+			index.un_hide()
+			ajuda.hide()
+		if window == index and event == 'Confirm':
+			search = values['word']
+			threading.Thread(target=retweet, args=(search,), daemon=True).start()
